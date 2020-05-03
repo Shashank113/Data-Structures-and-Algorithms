1f79bc4fd366eec27e81fba29b78a3bab05da763
Shashank <k.shashank.112@gmail.com>
2020-05-03 19:34:35 +0530

    Linear Search refactored

--- src/com/shashank/searchingAlgorithms/LinearSearch.java
+++ src/com/shashank/searchingAlgorithms/LinearSearch.java
@@ -3,7 +3,7 @@ package com.shashank.searchingAlgorithms;
 public class LinearSearch {
 
     public int linearSearch(int[] inputArray, int key){
-        for(int position = 0; position < inputArray.length; i++){
+        for(int position = 0; position < inputArray.length; position++){
             if(inputArray[position] == key){
                 return position + 1;
             }
